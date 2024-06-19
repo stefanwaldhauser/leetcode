c87c09d3445e33be87cb15d5fa13dbb86cac5370
Stefan Waldhauser <stefan.waldhauser@proton.me>
2024-06-19 22:04:18 +0200

    Solved max sum of distinct subarrays of size k

---
+++ SlidingWindow/easy/lc_max_sum_of_distinct_subarrays_k.py
@@ -0,0 +1,48 @@
+# https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/
+
+
+# integer array nums ex: [1,5,4,2,9,9,9]
+# integer k ex: 3
+# Find the maximum subarray sum of all subarrays that are of exactly length k, and have distinct elements
+# Return 0 if none such subarrays exist
+
+from collections import deque, defaultdict  # Import deque for efficient sliding window and defaultdict for automatic dictionary initialization
+from typing import List  # Import List for type hinting
+
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        max_sum = 0
+
+        # Create a deque to represent the sliding window. O(1) to pop from left!
+        # .remove in a list is O(n) in comparison
+        window = deque()
+        window_sum = 0
+
+        # We will use a dict to efficiently check for duplicates in O(1), namely if the number of keys in this dict is equal to k
+        # If not, the window is either not yet at size k OR we have found a duplicate and removed the key
+        # Will contain the count of all elements currently in the window.
+        # The trick is that the number of keys will be less than k if any element has a count different than 1, meaning a duplicate
+        window_count = defaultdict(int)
+
+        for v in nums:
+            window.append(v)
+            window_sum += v
+            window_count[v] += 1 # This is no problem as we use defaultdict. If the key does not exist yet, it will be set to 0 before adding 1
+
+            if len(window) > k:
+                oldest_element = window.popleft()
+                window_sum -= oldest_element
+                window_count[oldest_element] -= 1
+                if window_count[oldest_element] == 0: # All occurrences of that element removed from window, remove key
+                    del window_count[oldest_element]
+
+            # This checks if we have a window that has grown to size k and has no duplicates
+            if len(window) == k and len(window_count) == k:
+                max_sum = max(max_sum, window_sum)
+
+        return max_sum
+
+
+
+# Run Time: O(n)
+# Space Complexity: O(k)
